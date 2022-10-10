a60755d1edd432cba812d833cc3c46ee4e1a5d28
Ngoc <12643334-ngoc.n4g@users.noreply.gitlab.com>
2022-10-11 01:53:11 +0700

    add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
+import { Header } from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Header></Header>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
   )
 }
 
