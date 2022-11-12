20b63a33c3796a92612e863c07700f66da1f3e19
Ngoc <12643334-ngoc.n4g@users.noreply.gitlab.com>
2022-11-13 01:27:50 +0700

    move authentication function to Auth

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,15 +7,11 @@ import {
   Squares2X2Icon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
-import { useWeb3React } from '@web3-react/core'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Fragment, useEffect, useState } from 'react'
+import { Fragment } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { injectedConnector } from '~/config'
-import { givabitApi } from '~/services/givabit/api'
 import { formatWalletAddress } from '~/utils/wallet'
-import { signMessage } from '~/utils/web3'
 import Logo from '../../public/img/givabit_logo.jpg'
 import { useAuthSlice } from './Auth/slice'
 import { selectAuth } from './Auth/slice/selectors'
@@ -74,33 +70,14 @@ const Header: React.FC<any> = () => {
   const { wallet } = useSelector(selectAuth)
   const { actions } = useAuthSlice()
   const dispatch = useDispatch()
-  const { active, account, connector, activate, error } = useWeb3React()
-  const [isSigningInUsingWallet, isSigningInUsingWalletSetter] = useState(false)
 
-  async function signOut() {
+  function signOut() {
     dispatch(actions.signOut())
   }
 
-  useEffect(() => {
-    ;(async () => {
-      if (isSigningInUsingWallet) {
-        try {
-          if (!connector || !account) {
-            await activate(injectedConnector, undefined, true)
-          } else {
-            const nonce = await givabitApi.getNonce(account)
-            const signResponse = await signMessage(connector, nonce, account)
-            const loginInfo = await givabitApi.loginUsingWallet(account, signResponse)
-            dispatch(actions.signedIn(loginInfo.user))
-            isSigningInUsingWalletSetter(false)
-          }
-        } catch (e) {
-          isSigningInUsingWalletSetter(false)
-        }
-      }
-    })()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isSigningInUsingWallet, connector, account])
+  function signIn() {
+    dispatch(actions.setSigningIn(true))
+  }
 
   return (
     <Popover className="fixed z-30 w-full bg-white shadow-md">
@@ -197,7 +174,7 @@ const Header: React.FC<any> = () => {
                 href="#"
                 className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-n4gMediumTeal px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-n4gDarkTeal"
                 onClick={() => {
-                  isSigningInUsingWalletSetter(true)
+                  signIn()
                 }}
               >
                 Connect
