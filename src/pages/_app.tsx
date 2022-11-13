3baa485780788590773f51ee08c94b0282096764
Ngoc <12643334-ngoc.n4g@users.noreply.gitlab.com>
2022-11-13 17:52:44 +0700

    fix warning

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,14 @@ import * as React from 'react'
 import { HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'react-hot-toast'
 import { Provider } from 'react-redux'
+import { useIsomorphicLayoutEffect } from 'usehooks-ts'
 import Web3 from 'web3'
 import { Auth } from '~/components/Auth/Auth'
 import { configureAppStore } from '../store/configureStore'
 import '../styles/globals.css'
+
+// @ts-ignore
+React.useLayoutEffect = useIsomorphicLayoutEffect
 
 function getLibrary(provider: any) {
   return new Web3(provider)
