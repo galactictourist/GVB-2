d49c336e92cea4972bc1a82df73091036b37886d
Peter Novak <peter.novak.m@gmail.com>
2022-11-20 21:46:34 +0800

    A simple layout of a profile page

---
+++ src/components/Profile/CollectionRow.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+
+interface Props {
+  name: string
+  description: string
+  cause: string
+  image: string
+}
+
+const CollectionRow = ({ name, description, cause, image }: Props) => {
+  return (
+    <div className="mb-4 rounded-lg border bg-gray-50 p-2">
+      <div className="space-x-6 py-4 sm:flex">
+        <div className="ml-6 sm:ml-2">
+          <Image
+            src={image}
+            alt="Collection image"
+            width={120}
+            height={120}
+            className="ml-2 rounded-xl"
+          />
+        </div>
+        <div className="flex-auto space-y-1">
+          <h2 className="pt-4 text-xl">{name}</h2>
+          <h4 className="pt-2">{description}</h4>
+          <h4 className="pt-2 uppercase text-gray-500">{cause}</h4>
+        </div>
+        <div className="mt-auto pt-4">
+          <button className="flex h-10 w-24 items-center justify-center rounded-md border border-transparent bg-n4gMediumTeal px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-n4gDarkTeal">
+            Edit
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CollectionRow
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,8 +1,28 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import CollectionRow from '~/components/Profile/CollectionRow'
+import ProfileOverview from '~/components/Profile/ProfileOverview'
 import Header from '../../components/Header'
 
+const collections = [
+  {
+    name: 'First collection',
+    description: 'My first awesome collection',
+    cause: 'Education',
+    image: '/img/1.png',
+  },
+  {
+    name: 'Second collection',
+    description: 'The just as great second collection',
+    cause: 'Education',
+    image: '/img/2.png',
+  },
+]
+
 const Profile: NextPage = () => {
+  const router = useRouter()
+
   return (
     <>
       <Head>
@@ -12,7 +32,29 @@ const Profile: NextPage = () => {
 
       <Header />
 
-      <div className="p-20">PROFILE PAGE</div>
+      <div className="mx-auto max-w-2xl px-10 pt-32 lg:max-w-7xl">
+        <ProfileOverview />
+        <div className="border-t">
+          <h2 className="py-4 text-2xl">Current collections</h2>
+          {collections.map((collection) => (
+            <CollectionRow
+              key={collection.name}
+              name={collection.name}
+              description={collection.description}
+              cause={collection.cause}
+              image={collection.image}
+            />
+          ))}
+        </div>
+        <div className="flex justify-end pr-2">
+          <button
+            className="flex h-10 w-24 items-center justify-center rounded-md border border-transparent bg-n4gMediumTeal px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-n4gDarkTeal"
+            onClick={() => router.push('/collection/create')}
+          >
+            Create
+          </button>
+        </div>
+      </div>
     </>
   )
 }
