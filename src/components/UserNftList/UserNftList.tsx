803083f0754050ae07598bbf85ea222e75f08236
Ngoc <12643334-ngoc.n4g@users.noreply.gitlab.com>
2023-01-09 05:04:28 +0700

    check token id

--- src/components/UserNftList/UserNftList.tsx
+++ src/components/UserNftList/UserNftList.tsx
@@ -272,17 +272,19 @@ export function UserNftList() {
                   </a> */}
                 </div>
                 <div className="-ml-px flex w-0 flex-1">
-                  <a
-                    href={`#`}
-                    onClick={(e) => {
-                      sellingNftsSetter([nft])
-                      e.preventDefault()
-                    }}
-                    className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
-                  >
-                    <PhoneIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                    <span className="ml-3">Sell</span>
-                  </a>
+                  {nft.tokenId && (
+                    <a
+                      href={`#`}
+                      onClick={(e) => {
+                        sellingNftsSetter([nft])
+                        e.preventDefault()
+                      }}
+                      className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
+                    >
+                      <PhoneIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                      <span className="ml-3">Sell</span>
+                    </a>
+                  )}
                 </div>
               </div>
             </div>
